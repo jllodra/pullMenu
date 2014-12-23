ffb1401cab5a7b4faccf0e61ce192f3fcc36351d
Ruben Nine <ruben@troikalabs.com>
2014-12-23 19:06:48 +0100

    Adding basic constraints for menu and content views

--- pullMenu/PullMenuViewController.swift
+++ pullMenu/PullMenuViewController.swift
@@ -26,13 +26,51 @@ class PullMenuViewController: UIViewController {
         view.addSubview(menuView)
         view.addSubview(contentView)
     }
+
+    private func setupConstraints()
+    {
+        menuView.autoSetDimension(
+            ALDimension.Width,
+            toSize: view.bounds.width
+        )
+        
+        menuView.autoPinEdgeToSuperviewEdge(ALEdge.Top)
+        menuView.autoPinEdgeToSuperviewEdge(ALEdge.Leading)
+        menuView.autoPinEdgeToSuperviewEdge(ALEdge.Trailing)
+        
+        contentView.autoSetDimension(
+            ALDimension.Width,
+            toSize: view.bounds.width
+        )
+
+        contentView.autoPinEdge(
+            ALEdge.Top,
+            toEdge: ALEdge.Bottom,
+            ofView: menuView
+        )
+
+        contentView.autoMatchDimension(
+            ALDimension.Height,
+            toDimension: ALDimension.Height,
+            ofView: menuView
+        )
+
+        contentView.autoPinEdgeToSuperviewEdge(ALEdge.Bottom)
+        contentView.autoPinEdgeToSuperviewEdge(ALEdge.Leading)
+        contentView.autoPinEdgeToSuperviewEdge(ALEdge.Trailing)
+    }
     
     private func debug() {
         view.backgroundColor = UIColor.blackColor()
+        menuView.backgroundColor = UIColor.greenColor()
+        contentView.backgroundColor = UIColor.blueColor()
+        
+        println("view.subviews = %d", view.subviews)
     }
     
     override func viewDidLoad() {
         addControls()
+        setupConstraints()
         debug()
     }
 
