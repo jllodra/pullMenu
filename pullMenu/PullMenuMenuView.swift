29aae1a6482f269f823215dd508cca890073524d
Ruben Nine <ruben@troikalabs.com>
2014-12-23 18:47:07 +0100

    Adding Bridging Header, PullMenuViewController and PullMenuMenuView

---
+++ pullMenu/PullMenuMenuView.swift
@@ -0,0 +1,13 @@
+//
+//  PullMenuMenuView.swift
+//  pullMenu
+//
+//  Created by Ruben on 12/23/14.
+//  Copyright (c) 2014 Ruben Nine & Josep Llodra. All rights reserved.
+//
+
+import UIKit
+
+class PullMenuMenuView: UIView {
+
+}
---
+++ pullMenu/PullMenuViewController.swift
@@ -0,0 +1,34 @@
+//
+//  PullMenuViewController.swift
+//  pullMenu
+//
+//  Created by Ruben on 12/23/14.
+//  Copyright (c) 2014 Ruben Nine & Josep Llodra. All rights reserved.
+//
+
+import UIKit
+
+class PullMenuViewController: UIViewController {
+
+    private lazy var menuView: PullMenuMenuView = {
+        let obj = PullMenuMenuView(forAutoLayout: ())
+        
+        return obj
+    }()    
+
+    private lazy var contentView: UIView = {
+        let obj = UIView(forAutoLayout: ())
+        
+        return obj
+    }()
+
+    private func addControls() {
+        view.addSubview(menuView)
+        view.addSubview(contentView)
+    }
+    
+    override func viewDidLoad() {
+        addControls()
+    }
+
+}
